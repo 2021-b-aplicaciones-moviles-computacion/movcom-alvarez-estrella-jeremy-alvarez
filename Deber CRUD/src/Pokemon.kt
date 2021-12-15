a2b7c2645619b55d3f6a1915487e6616de6b5a19
Alvarez Jeremy <jeremy.alvarez@epn.edu.ec>
2021-12-15 17:38:39 -0500

    Clase Pokemon

--- Deber CRUD/src/Pokemon.kt
+++ Deber CRUD/src/Pokemon.kt
@@ -1,2 +1,72 @@
-class Pokemon {
+
+
+class Pokemon (
+    private var idPokemon: Int = 0,
+    private var nombre_pokemon: String = "",
+    private var shiny: Boolean = true,
+    private var nivel: Int = 1,
+    private var habilidad: String = "",
+    private var estadisticas: Array<Int> = arrayOf(0,0,0,0,0,0),
+    private var trainer: Int = 0
+){
+    init {
+
+    }
+    //-------GETTERS-------------------
+    public fun getIdPokemon(): Int{
+        return this.idPokemon
+    }
+
+    public fun getNombre_Pokemon(): String{
+        return this.nombre_pokemon
+    }
+
+    public fun getShiny(): Boolean{
+        return this.shiny
+    }
+
+    public fun getNivel(): Int{
+        return this.nivel
+    }
+
+    public fun getHabilidad(): String{
+        return this.habilidad
+    }
+
+    public fun getEstadisticas(): Array<Int>{
+        return this.estadisticas
+    }
+
+    public fun getTrainer(): Int{
+        return this.trainer
+    }
+
+    //-----------SETTERS------------------
+    public fun setIdPokemon(idPok: Int){
+        this.idPokemon = idPok
+    }
+
+    public fun setNombre_Pokemon(nombre: String){
+        this.nombre_pokemon = nombre
+    }
+
+    public fun setShiny(shi: Boolean){
+        this.shiny = shi
+    }
+
+    public fun setNivel(lvl:Int){
+        this.nivel = lvl
+    }
+
+    public fun setHabilidad(hab:String){
+        this.habilidad=hab
+    }
+
+    public fun setEstadisticas(stats: Array<Int>){
+        this.estadisticas = stats
+    }
+
+    public fun setTrainer(entrenador: Int){
+        this.trainer = entrenador
+    }
 }
--- Deber CRUD/src/Trainer.kt
+++ Deber CRUD/src/Trainer.kt
@@ -1,2 +1,13 @@
-class Trainer {
+import java.util.*
+
+class Trainer (
+    private var id_trainer: Int = 0,
+    private var nombre_trainer: String = "",
+    private var genero_trainer:String = "",
+    private var dinero: Float = 0F,
+    private var juego_iniciado: Date = Date()
+){
+    init {
+
+    }
 }
