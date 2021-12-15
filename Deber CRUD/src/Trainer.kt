c7be9e98729b9de69ee0fcfddc13ff6e08493486
Alvarez Jeremy <jeremy.alvarez@epn.edu.ec>
2021-12-15 18:05:47 -0500

    Clase Trainer

--- Deber CRUD/src/Trainer.kt
+++ Deber CRUD/src/Trainer.kt
@@ -10,4 +10,46 @@ class Trainer (
     init {
 
     }
+
+    //-------GETTERS-------------------
+    public fun getIdTrainer(): Int{
+        return this.id_trainer
+    }
+
+    public fun getnombreTrainer(): String{
+        return this.nombre_trainer
+    }
+
+    public fun getGeneroTrainer(): String{
+        return this.genero_trainer
+    }
+
+    public fun getDinero(): Float{
+        return this.dinero
+    }
+
+    public fun getJuegoInit(): Date{
+        return this.juego_iniciado
+    }
+
+    //-----------SETTERS------------------
+    public fun setIdTrainer(idTra: Int){
+        this.id_trainer = idTra
+    }
+
+    public fun setNombreTrainer(nombreTra: String){
+        this.nombre_trainer = nombreTra
+    }
+
+    public fun setGeneroTrainer(genero: String){
+        this.genero_trainer = genero
+    }
+
+    public fun setDinero(dinero: Float){
+        this.dinero = dinero
+    }
+
+    public fun setJuegoIniciado(gameStart: Date){
+        this.juego_iniciado = gameStart
+    }
 }
